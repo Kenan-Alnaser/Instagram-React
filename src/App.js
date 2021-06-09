efe37a4f61a5eba022e301ca6c35bb95ff18dfa2
Kenan Alnaser <kenan-na@live.com>
2021-06-09 14:07:38 +0200

    half way done perhaps

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 import Data from "./data.json";
+import "./sass/main.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   // const [posts, setPosts] = useState(Data);
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,33 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Gallery = ({ Data }) => {
   console.log(Data);
-  return <div></div>;
+  const info = Data.map((obj, i) => {
+    const { id, title, location, text, likes, comments, img } = obj;
+    return (
+      <div class="container" key={id}>
+        <div class="gallery">
+          <div class="gallery-item" tabindex="0">
+            <img src={img} class="gallery-image" alt="" />
+
+            <div class="gallery-item-info">
+              <ul>
+                <li class="gallery-item-likes">
+                  <span class="visually-hidden">Likes:</span>
+                  <i class="fas fa-heart" aria-hidden="true"></i> {likes}
+                </li>
+                <li class="gallery-item-comments">
+                  <span class="visually-hidden">Comments:</span>
+                  <i class="fas fa-comment" aria-hidden="true"></i> {comments}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
+  return <React.Fragment>{info}</React.Fragment>;
 };
 export default Gallery;
